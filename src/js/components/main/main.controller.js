c98bf32f71ce88d18ed879b41955d29cfec9924d
Ryan Harings <ryanharings@hotmail.com>
2016-12-03 12:15:47 -0700

    writing filter algorithm

--- src/js/components/main/main.controller.js
+++ src/js/components/main/main.controller.js
@@ -12,32 +12,51 @@
     /*jshint validthis: true */
     this.greeting = 'This.Is.Snurfer!';
 
-    this.snowVids = contentService.allVideos.snow;
-    this.surfVids = contentService.allVideos.surf;
+    this.contentList = [];
 
-    $scope.slider = {
-      value: 2,
+    this.slider = {
+      value: 'Snurf',
       options: {
+        stepsArray: ['Pow Day', 'Shred', 'Snurf', 'Surfs Up', 'Barrels'],
         showSelectionBar: true,
         getPointerColor: function(value) {
-          if (value <= 3)
+          if (value === 'Barrels')
             return '#305CB2';
-          if (value <= 6)
+          if (value === 'Surfs Up')
+            return '#305CB2';
+          if (value === 'Snurf')
             return '#23A6B2';
-          if (value <= 9)
+          if (value === 'Shred')
             return '#E0FFFF';
-          return '#E0FFFF';
+          if (value === 'Pow Day')
+            return '#E0FFFF';
         },
-        translate: function(value, sliderId, label) {
-          switch (label) {
-            case 'model':
-              return '<b>Min price:</b> $' + value;
-            case 'high':
-              return '<b>Max price:</b> $' + value;
-            default:
-              return '$' + value
-          }
-        }
+        showTicksValues: false
+      }
+    };
+
+    this.initialContent = () => {
+      for (let i = 0; i < contentService.allVideos.snow.length; i++) {
+        this.contentList.push(
+          contentService.allVideos.snow[i],
+          contentService.allVideos.surf[i]
+        );
+      }
+    };
+    this.initialContent();
+
+    this.slideToInt = () => {
+      var slideVal = this.slider.value;
+      if (slideVal === 'Pow Day') {
+        return 1;
+      } else if (slideVal === 'Shred') {
+        return 2;
+      } else if (slideVal === 'Snurf') {
+        return 3;
+      } else if (slideVal === 'Surfs Up') {
+        return 4;
+      } else if (slideVal === 'Barrels') {
+        return 5;
       }
     };
 
