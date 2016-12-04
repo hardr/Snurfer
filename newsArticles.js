aa6bd3a0ff55e3d65ac4d2f2d634e4b93e2a3c08
Ryan Harings <ryanharings@hotmail.com>
2016-12-04 00:36:17 -0700

    added news data

---
+++ newsArticles.js
@@ -0,0 +1,90 @@
+window.news = {
+  snow: [
+    {
+      id: 31,
+      type: 'snow',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/263139931/263651522',
+      title: 'Sibling Snowboarders Hope To Reach Olympics At The Same Time'
+    },
+    {
+      id: 32,
+      type: 'snow',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/264531379/264742960',
+      title: 'Zen And The Art Of Snowboarding: Jamie Anderson'
+    },
+    {
+      id: 33,
+      type: 'snow',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/282836515/282836516',
+      title: 'Researchers Debate Effectiveness Of Snow Helmets'
+    },
+    {
+      id: 34,
+      type: 'snow',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/263139931/263651522',
+      title: 'Sibling Snowboarders Hope To Reach Olympics At The Same Time'
+    },
+    {
+      id: 35,
+      type: 'snow',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/264531379/264742960',
+      title: 'Zen And The Art Of Snowboarding: Jamie Anderson'
+    },
+    {
+      id: 36,
+      type: 'snow',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/282836515/282836516',
+      title: 'Researchers Debate Effectiveness Of Snow Helmets'
+    }
+  ],
+  surf: [
+    {
+      id: 41,
+      type: 'surf',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/503055233/503108978',
+      title: 'Women Take On Big-Wave Surfing, Once The Domain Of Men, At Mavericks'
+    },
+    {
+      id: 42,
+      type: 'surf',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/499995282/500898944',
+      title: 'Gaza\'s Surfer Girl Hangs Up Her Board — And Not By Choice'
+    },
+    {
+      id: 43,
+      type: 'surf',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/490545030/491906551',
+      title: 'Surfers And Scientists Team Up To Create The \'Perfect Wave\''
+    },
+    {
+      id: 44,
+      type: 'surf',
+      content: 'news',
+      url: 'https://www.npr.org/player/embed/366600463/367938729',
+      title: 'If Everybody Had An Ocean, Could We Surf Our Way To Mental Health?'
+    },
+    {
+      id: 45,
+      type: 'surf',
+      content: 'news',
+      url: '<iframe src="https://www.npr.org/player/embed/347950272/348010338" width="100%" height="290" frameborder="0" scrolling="no" title="NPR embedded audio player"></iframe>',
+      title: 'Riding The \'Silver Dragon,\' Surfers Tame China\'s 10-Foot River Waves'
+    },
+    {
+      id: 46,
+      type: 'surf',
+      content: 'news',
+      url: 'http://www.surfline.com/surf-news/minimal-swell-makes-for-desperate-suhttps://www.npr.org/player/embed/499995282/500898944',
+      title: 'Scary Small at Sunset Opening Day'
+    }
+  ]
+};
