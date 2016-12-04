e85b668fd8da4bcee31710cb835c14635aaad7e5
Ryan Harings <ryanharings@hotmail.com>
2016-12-03 17:28:07 -0700

    percentage logic in place

--- js/components/main/main.controller.js
+++ js/components/main/main.controller.js
@@ -31,34 +31,82 @@
           if (value === 'Pow Day')
             return '#E0FFFF';
         },
-        showTicksValues: false
+        showTicksValues: false,
+        onChange: () => {
+          this.contentList = [];
+          $scope.listContent()
+        }
       }
     };
 
-    this.initialContent = () => {
-      for (let i = 0; i < contentService.allVideos.snow.length; i++) {
-        this.contentList.push(
-          contentService.allVideos.snow[i],
-          contentService.allVideos.surf[i]
-        );
-      }
-    };
-    this.initialContent();
-
-    this.slideToInt = () => {
+    $scope.listContent = () => {
       var slideVal = this.slider.value;
-      if (slideVal === 'Pow Day') {
-        return 1;
+      console.log(slideVal);
+      if (slideVal === 'Snurf') {
+        for (let i = 0; i < contentService.allVideos.snow.length; i++) {
+          this.contentList.push(
+            contentService.allVideos.snow[i],
+            contentService.allVideos.surf[i]
+          );
+        }
+      } else if (slideVal === 'Pow Day') {
+        for (let i = 0; i < contentService.allVideos.snow.length; i++) {
+          this.contentList.push(
+            contentService.allVideos.snow[i]
+          );
+        }
       } else if (slideVal === 'Shred') {
-        return 2;
-      } else if (slideVal === 'Snurf') {
-        return 3;
+        var surfCount = 0;
+        for (let i = 0; i < contentService.allVideos.snow.length; i += 3) {
+          this.contentList.push(
+            contentService.allVideos.snow[i],
+            contentService.allVideos.snow[i + 1],
+            contentService.allVideos.snow[i + 2],
+            contentService.allVideos.surf[surfCount]
+          );
+          surfCount++;
+        }
       } else if (slideVal === 'Surfs Up') {
-        return 4;
+        var snowCount = 0;
+        for (let i = 0; i < contentService.allVideos.surf.length; i += 3) {
+          this.contentList.push(
+            contentService.allVideos.surf[i],
+            contentService.allVideos.surf[i + 1],
+            contentService.allVideos.surf[i + 2],
+            contentService.allVideos.snow[snowCount]
+          );
+          snowCount++;
+        }
       } else if (slideVal === 'Barrels') {
-        return 5;
+        for (let i = 0; i < contentService.allVideos.surf.length; i++) {
+          this.contentList.push(
+            contentService.allVideos.surf[i]
+          );
+        }
       }
     };
+
+    $scope.listContent();
+
+    // $scope.$watch('test', () => {
+    //   console.log('!');
+    //   // this.listContent()
+    // })
+    //
+    // this.slideToInt = () => {
+    //   var slideVal = this.slider.value;
+    //   if (slideVal === 'Pow Day') {
+    //     return 1;
+    //   } else if (slideVal === 'Shred') {
+    //     return 2;
+    //   } else if (slideVal === 'Snurf') {
+    //     return 3;
+    //   } else if (slideVal === 'Surfs Up') {
+    //     return 4;
+    //   } else if (slideVal === 'Barrels') {
+    //     return 5;
+    //   }
+    // };
 
   }
 
