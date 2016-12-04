46702ffd42abf451577385f455e4b7e62b271118
Ryan Harings <ryanharings@hotmail.com>
2016-12-03 17:28:07 -0700

    percentage logic in place

--- src/js/filters.js
+++ src/js/filters.js
@@ -10,8 +10,37 @@
   // slideVideoFilter.$inject = ['$http'];
 
   function slideVideoFilter() {
-    return function(number) {
-      console.log(number);
+
+    return function(content, slideVal) {
+      var snowCount = 0;
+      var surfCount = 0;
+      return filtered = content.filter((video, i) => {
+
+        if (slideVal === 'Snurf') {
+          return video;
+        } else if (slideVal === 'Pow Day') {
+          return video.type === 'snow';
+        } else if (slideVal === 'Shred') {
+          console.log(snowCount);
+          console.log(surfCount);
+          if (video.type === 'snow') {
+            snowCount++;
+            return video;
+          } else if (video.type === 'surf' && surfCount === 0) {
+            console.log(surfCount);
+            surfCount++;
+            console.log(surfCount);
+            return video;
+          } else if (video.type === 'surf' && snowCount % surfCount === 3) {
+            surfCount++;
+            return video;
+          }
+        } else if (slideVal === 'Surfs Up') {
+          return ;
+        } else if (slideVal === 'Barrels') {
+          return video.type === 'surf';
+        }
+      })
     };
   }
 
