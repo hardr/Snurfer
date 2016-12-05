98a89de681abd2444613fda8783dea19af36a489
Ryan Harings <ryanharings@hotmail.com>
2016-12-05 09:13:22 -0700

    weather api in place on page

--- src/js/services.js
+++ src/js/services.js
@@ -10,6 +10,15 @@
   function contentService($http, $window) {
     this.allVideos = window.videoSamples;
     this.allNews = window.news;
+
+    this.weatherBaseUrl = 'http://api.wunderground.com/api/9c007c8680fe69c5/';
+
+    // this.getCurrentWeather = function(city, state) {
+    //   return $http.get(this.weatherBaseUrl + `conditions/q/${state || 'CO'}/${city || 'Denver'}.json`);
+    // };
+    this.getForecastWeather = function(city, state) {
+      return $http.get(this.weatherBaseUrl + `forecast/q/${state || 'CO'}/${city || 'Denver'}.json`);
+    };
   }
 
 }());
