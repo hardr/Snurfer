a97b4417b92b4754c8b53ca3afb36c8cf30b5227
Ryan Harings <ryanharings@hotmail.com>
2016-12-06 19:50:17 -0700

    api connected, content and user get route

--- js/components/main/main.controller.js
+++ js/components/main/main.controller.js
@@ -10,219 +10,238 @@
 
   function mainController($scope, contentService) {
     /*jshint validthis: true */
-    this.greeting = 'This.Is.Snurfer!';
-    this.contentList = [];
-    this.location = {
-      updateWeather: () => {
-        contentService.getForecastWeather(this.location.city, this.location.state)
-        .then((forecast) => {
+    this.allContent = contentService.allContent()
+      .then(content => {
+        content.data.forEach((each) => {
+          if (each.type === 'snow')
+            this.snowList.push(each);
+          if (each.type === 'skate')
+            this.skateList.push(each);
+          if (each.type === 'surf')
+            this.surfList.push(each);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-          var forecastData = forecast.data.forecast.simpleforecast.forecastday;
-          this.forecastWeather = forecastData;
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-      }
-    };
+    this.snowList = [];
+    this.skateList = [];
+    this.surfList = [];
+
+    // this.location = {
+    //   updateWeather: () => {
+    //     contentService.getForecastWeather(this.location.city, this.location.state)
+    //     .then((forecast) => {
+    //
+    //       var forecastData = forecast.data.forecast.simpleforecast.forecastday;
+    //       this.forecastWeather = forecastData;
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    //   }
+    // };
     // this.currentWeather = {};
-    this.forecastWeather = {};
+    // this.forecastWeather = {};
+
+    var snowColor = '#C1FF88';
+    var skateColor = '#CC7C63';
+    var surfColor = '#7F98FF';
+    var currentColor = surfColor;
 
     this.typeSlider = {
-      value: 'Snurf',
+      value: 'Surf',
       options: {
-        stepsArray: ['Pow Day', 'Shred', 'Snurf', 'Surfs Up', 'Barrels'],
+        stepsArray: ['Snow', 'Skate', 'Surf'],
         showSelectionBar: true,
         getPointerColor: function(value) {
-          if (value === 'Barrels')
-            return '#305CB2';
-          if (value === 'Surfs Up')
-            return '#305CB2';
-          if (value === 'Snurf')
-            return '#23A6B2';
-          if (value === 'Shred')
-            return '#E0FFFF';
-          if (value === 'Pow Day')
-            return '#E0FFFF';
+          if (value === 'Snow') {
+            currentColor = snowColor;
+            return snowColor;
+          }
+          if (value === 'Skate') {
+            currentColor = skateColor;
+            return skateColor;
+          }
+          if (value === 'Surf') {
+            currentColor = surfColor;
+            return surfColor;
+          }
         },
-        showTicksValues: false,
-        onChange: () => {
-          this.contentList = [];
-          $scope.listContent();
-        }
+        showTicksValues: false
+        // onChange: () => {
+        //   this.contentList = [];
+        //   $scope.listContent();
+        // }
       }
     };
 
     this.contentSlider = {
-      value: 'A bit of both',
+      value: 'All',
       options: {
-        stepsArray: ['Videos', 'A bit of both', 'News'],
+        stepsArray: ['Videos', 'All', 'Photos'],
         showSelectionBar: true,
-        getPointerColor: function(value) {
-          if (value === 'Videos')
-            return '#305CB2';
-          if (value === 'A bit of both')
-            return '#23A6B2';
-          if (value === 'News')
-            return '#E0FFFF';
+        getPointerColor: (value) => {
+          return currentColor;
         },
-        showTicksValues: true,
-        onChange: () => {
-          this.contentList = [];
-          $scope.listContent();
-        }
+        showTicksValues: false
+        // onChange: () => {
+        //   this.contentList = [];
+        //   $scope.listContent();
+        // }
       }
     };
 
-    $scope.listContent = () => {
-      var typeSlideVal = this.typeSlider.value;
-      var contentSlideVal = this.contentSlider.value;
+    // $scope.listContent = () => {
+    //   var typeSlideVal = this.typeSlider.value;
+    //   var contentSlideVal = this.contentSlider.value;
+    //
+    //   if (typeSlideVal === 'Snurf') {
+    //     if (contentSlideVal === 'Videos') {
+    //       for (let i = 0; i < contentService.allVideos.snow.length; i++) {
+    //         this.contentList.push(
+    //           contentService.allVideos.snow[i],
+    //           contentService.allVideos.surf[i]
+    //         );
+    //       }
+    //     } else if (contentSlideVal === 'A bit of both') {
+    //       for (let i = 0; i < contentService.allVideos.snow.length; i++) {
+    //         this.contentList.push(
+    //           contentService.allVideos.snow[i],
+    //           contentService.allVideos.surf[i],
+    //           contentService.allNews.snow[i],
+    //           contentService.allNews.surf[i]
+    //         );
+    //       }
+    //     } else if (contentSlideVal === 'News') {
+    //       for (let i = 0; i < contentService.allVideos.snow.length; i++) {
+    //         this.contentList.push(
+    //           contentService.allNews.snow[i],
+    //           contentService.allNews.surf[i]
+    //         );
+    //       }
+    //     }
+    //   } else if (typeSlideVal === 'Pow Day') {
+    //     if (contentSlideVal === 'Videos') {
+    //       for (let i = 0; i < contentService.allVideos.snow.length; i++) {
+    //         this.contentList.push(
+    //           contentService.allVideos.snow[i]
+    //         );
+    //       }
+    //     } else if (contentSlideVal === 'A bit of both') {
+    //       for (let i = 0; i < contentService.allVideos.snow.length; i++) {
+    //         this.contentList.push(
+    //           contentService.allVideos.snow[i],
+    //           contentService.allNews.snow[i]
+    //         );
+    //       }
+    //     } else if (contentSlideVal === 'News') {
+    //       for (let i = 0; i < contentService.allVideos.snow.length; i++) {
+    //         this.contentList.push(
+    //           contentService.allNews.snow[i]
+    //         );
+    //       }
+    //     }
+    //   } else if (typeSlideVal === 'Shred') {
+    //     var surfCount = 0;
+    //     if (contentSlideVal === 'Videos') {
+    //       for (let i = 0; i < contentService.allVideos.snow.length; i += 3) {
+    //         this.contentList.push(
+    //           contentService.allVideos.snow[i],
+    //           contentService.allVideos.surf[surfCount],
+    //           contentService.allVideos.snow[i + 1],
+    //           contentService.allVideos.snow[i + 2]
+    //         );
+    //         surfCount++;
+    //       }
+    //     } else if (contentSlideVal === 'A bit of both') {
+    //       for (let i = 0; i < contentService.allVideos.snow.length; i += 3) {
+    //         this.contentList.push(
+    //           contentService.allVideos.snow[i],
+    //           contentService.allVideos.surf[surfCount],
+    //           contentService.allNews.snow[i],
+    //           contentService.allVideos.snow[i + 1],
+    //           contentService.allNews.snow[i + 1],
+    //           contentService.allNews.surf[surfCount],
+    //           contentService.allVideos.snow[i + 2],
+    //           contentService.allNews.snow[i + 2]
+    //         );
+    //         surfCount++;
+    //       }
+    //     } else if (contentSlideVal === 'News') {
+    //       for (let i = 0; i < contentService.allVideos.snow.length; i += 3) {
+    //         this.contentList.push(
+    //           contentService.allNews.snow[i],
+    //           contentService.allNews.surf[surfCount],
+    //           contentService.allNews.snow[i + 1],
+    //           contentService.allNews.snow[i + 2]
+    //         );
+    //         surfCount++;
+    //       }
+    //     }
+    //   } else if (typeSlideVal === 'Surfs Up') {
+    //     var snowCount = 0;
+    //     if (contentSlideVal === 'Videos') {
+    //       for (let i = 0; i < contentService.allVideos.surf.length; i += 3) {
+    //         this.contentList.push(
+    //           contentService.allVideos.surf[i],
+    //           contentService.allVideos.snow[snowCount],
+    //           contentService.allVideos.surf[i + 1],
+    //           contentService.allVideos.surf[i + 2]
+    //         );
+    //         snowCount++;
+    //       }
+    //     } else if (contentSlideVal === 'A bit of both') {
+    //       for (let i = 0; i < contentService.allVideos.surf.length; i += 3) {
+    //         this.contentList.push(
+    //           contentService.allVideos.surf[i],
+    //           contentService.allVideos.snow[snowCount],
+    //           contentService.allNews.surf[i],
+    //           contentService.allVideos.surf[i + 1],
+    //           contentService.allNews.surf[i + 1],
+    //           contentService.allNews.snow[snowCount],
+    //           contentService.allVideos.surf[i + 2],
+    //           contentService.allNews.surf[i + 2]
+    //         );
+    //         snowCount++;
+    //       }
+    //     } else if (contentSlideVal === 'News') {
+    //       for (let i = 0; i < contentService.allVideos.surf.length; i += 3) {
+    //         this.contentList.push(
+    //           contentService.allNews.surf[i],
+    //           contentService.allNews.snow[snowCount],
+    //           contentService.allNews.surf[i + 1],
+    //           contentService.allNews.surf[i + 2]
+    //         );
+    //         snowCount++;
+    //       }
+    //     }
+    //   } else if (typeSlideVal === 'Barrels') {
+    //     if (contentSlideVal === 'Videos') {
+    //       for (let i = 0; i < contentService.allVideos.surf.length; i++) {
+    //         this.contentList.push(
+    //           contentService.allVideos.surf[i]
+    //         );
+    //       }
+    //     } else if (contentSlideVal === 'A bit of both') {
+    //       for (let i = 0; i < contentService.allVideos.surf.length; i++) {
+    //         this.contentList.push(
+    //           contentService.allVideos.surf[i],
+    //           contentService.allNews.surf[i]
+    //         );
+    //       }
+    //     } else if (contentSlideVal === 'News') {
+    //       for (let i = 0; i < contentService.allVideos.surf.length; i++) {
+    //         this.contentList.push(
+    //           contentService.allNews.surf[i]
+    //         );
+    //       }
+    //     }
+    //   }
+    // };
 
-      if (typeSlideVal === 'Snurf') {
-        if (contentSlideVal === 'Videos') {
-          for (let i = 0; i < contentService.allVideos.snow.length; i++) {
-            this.contentList.push(
-              contentService.allVideos.snow[i],
-              contentService.allVideos.surf[i]
-            );
-          }
-        } else if (contentSlideVal === 'A bit of both') {
-          for (let i = 0; i < contentService.allVideos.snow.length; i++) {
-            this.contentList.push(
-              contentService.allVideos.snow[i],
-              contentService.allVideos.surf[i],
-              contentService.allNews.snow[i],
-              contentService.allNews.surf[i]
-            );
-          }
-        } else if (contentSlideVal === 'News') {
-          for (let i = 0; i < contentService.allVideos.snow.length; i++) {
-            this.contentList.push(
-              contentService.allNews.snow[i],
-              contentService.allNews.surf[i]
-            );
-          }
-        }
-      } else if (typeSlideVal === 'Pow Day') {
-        if (contentSlideVal === 'Videos') {
-          for (let i = 0; i < contentService.allVideos.snow.length; i++) {
-            this.contentList.push(
-              contentService.allVideos.snow[i]
-            );
-          }
-        } else if (contentSlideVal === 'A bit of both') {
-          for (let i = 0; i < contentService.allVideos.snow.length; i++) {
-            this.contentList.push(
-              contentService.allVideos.snow[i],
-              contentService.allNews.snow[i]
-            );
-          }
-        } else if (contentSlideVal === 'News') {
-          for (let i = 0; i < contentService.allVideos.snow.length; i++) {
-            this.contentList.push(
-              contentService.allNews.snow[i]
-            );
-          }
-        }
-      } else if (typeSlideVal === 'Shred') {
-        var surfCount = 0;
-        if (contentSlideVal === 'Videos') {
-          for (let i = 0; i < contentService.allVideos.snow.length; i += 3) {
-            this.contentList.push(
-              contentService.allVideos.snow[i],
-              contentService.allVideos.surf[surfCount],
-              contentService.allVideos.snow[i + 1],
-              contentService.allVideos.snow[i + 2]
-            );
-            surfCount++;
-          }
-        } else if (contentSlideVal === 'A bit of both') {
-          for (let i = 0; i < contentService.allVideos.snow.length; i += 3) {
-            this.contentList.push(
-              contentService.allVideos.snow[i],
-              contentService.allVideos.surf[surfCount],
-              contentService.allNews.snow[i],
-              contentService.allVideos.snow[i + 1],
-              contentService.allNews.snow[i + 1],
-              contentService.allNews.surf[surfCount],
-              contentService.allVideos.snow[i + 2],
-              contentService.allNews.snow[i + 2]
-            );
-            surfCount++;
-          }
-        } else if (contentSlideVal === 'News') {
-          for (let i = 0; i < contentService.allVideos.snow.length; i += 3) {
-            this.contentList.push(
-              contentService.allNews.snow[i],
-              contentService.allNews.surf[surfCount],
-              contentService.allNews.snow[i + 1],
-              contentService.allNews.snow[i + 2]
-            );
-            surfCount++;
-          }
-        }
-      } else if (typeSlideVal === 'Surfs Up') {
-        var snowCount = 0;
-        if (contentSlideVal === 'Videos') {
-          for (let i = 0; i < contentService.allVideos.surf.length; i += 3) {
-            this.contentList.push(
-              contentService.allVideos.surf[i],
-              contentService.allVideos.snow[snowCount],
-              contentService.allVideos.surf[i + 1],
-              contentService.allVideos.surf[i + 2]
-            );
-            snowCount++;
-          }
-        } else if (contentSlideVal === 'A bit of both') {
-          for (let i = 0; i < contentService.allVideos.surf.length; i += 3) {
-            this.contentList.push(
-              contentService.allVideos.surf[i],
-              contentService.allVideos.snow[snowCount],
-              contentService.allNews.surf[i],
-              contentService.allVideos.surf[i + 1],
-              contentService.allNews.surf[i + 1],
-              contentService.allNews.snow[snowCount],
-              contentService.allVideos.surf[i + 2],
-              contentService.allNews.surf[i + 2]
-            );
-            snowCount++;
-          }
-        } else if (contentSlideVal === 'News') {
-          for (let i = 0; i < contentService.allVideos.surf.length; i += 3) {
-            this.contentList.push(
-              contentService.allNews.surf[i],
-              contentService.allNews.snow[snowCount],
-              contentService.allNews.surf[i + 1],
-              contentService.allNews.surf[i + 2]
-            );
-            snowCount++;
-          }
-        }
-      } else if (typeSlideVal === 'Barrels') {
-        if (contentSlideVal === 'Videos') {
-          for (let i = 0; i < contentService.allVideos.surf.length; i++) {
-            this.contentList.push(
-              contentService.allVideos.surf[i]
-            );
-          }
-        } else if (contentSlideVal === 'A bit of both') {
-          for (let i = 0; i < contentService.allVideos.surf.length; i++) {
-            this.contentList.push(
-              contentService.allVideos.surf[i],
-              contentService.allNews.surf[i]
-            );
-          }
-        } else if (contentSlideVal === 'News') {
-          for (let i = 0; i < contentService.allVideos.surf.length; i++) {
-            this.contentList.push(
-              contentService.allNews.surf[i]
-            );
-          }
-        }
-      }
-    };
-
-    $scope.listContent();
+    // $scope.listContent();
 
     // contentService.getCurrentWeather()
     // .then((weather) => {
@@ -231,14 +250,14 @@
     //   console.log(this.currentWeather);
     // });
 
-    contentService.getForecastWeather(location.city, location.state)
-    .then((forecast) => {
-      var forecastData = forecast.data.forecast.simpleforecast.forecastday;
-      this.forecastWeather = forecastData;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    // contentService.getForecastWeather(location.city, location.state)
+    // .then((forecast) => {
+    //   var forecastData = forecast.data.forecast.simpleforecast.forecastday;
+    //   this.forecastWeather = forecastData;
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
 
   }
 
